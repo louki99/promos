4a71020b9a2a1e41cd76aa152c34ef17c973431e
Louki99 <louki.idris@gmail.com>
2025-06-04 16:18:32 +0100

    add common dataaccess

---
+++ common/common-dataaccess/src/main/java/ma/foodplus/ordering/system/store/exception/StoreDataAccessException.java
@@ -0,0 +1,8 @@
+package ma.foodplus.ordering.system.store.exception;
+
+public class StoreDataAccessException extends RuntimeException{
+
+    public StoreDataAccessException(String message) {
+        super(message);
+    }
+}
