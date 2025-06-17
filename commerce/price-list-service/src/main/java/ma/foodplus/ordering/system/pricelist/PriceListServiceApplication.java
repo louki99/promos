4e1b34f46a2d3e14d86e201c8088f92b7531de61
Louki99 <louki.idris@gmail.com>
2025-06-17 21:23:33 +0100

    build infra structure and use groups bounded context

---
+++ commerce/price-list-service/src/main/java/ma/foodplus/ordering/system/pricelist/PriceListServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.foodplus.ordering.system.pricelist;
+
+/**
+ * Hello world!
+ *
+ */
+public class PriceListServiceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ commerce/price-list-service/src/test/java/ma/foodplus/ordering/system/PriceListServiceApplicationTest.java
@@ -0,0 +1,38 @@
+package ma.foodplus.ordering.system;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple PriceListServiceApplication.
+ */
+public class PriceListServiceApplicationTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PriceListServiceApplicationTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PriceListServiceApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
